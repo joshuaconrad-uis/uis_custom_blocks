2c087ac3775ae531bbbc776626908d26a85e23c4
joshua conrad <joshuaconrad04@gmail.com>
2025-05-09 14:59:26 -0500

    closer

--- uis_custom_blocks/uis_custom_blocks/js/src/editor.js
+++
@@ -1,7 +0,0 @@
-// Import all editor-only styles
-import './blocks/my-custom-block/editor.scss';
-import './blocks/uis-card/editor.scss';
-import './blocks/image_card/editor.scss';
-
-// console.log('Editor styles loaded');
-// Add other editor style imports as needed
